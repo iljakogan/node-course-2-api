21b0e6523c297c64123c22682bef104ef21a3c98
Ilja Kogan <ilja.kogan@gmail.com>
2017-06-22 20:45:43 +0000

    Adding testing

--- server/server.js
+++ server/server.js
@@ -39,4 +39,4 @@ app.get('/todos', (req, res) => {
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", () => {
     console.log('started');
 });
-//module.exports.app = app;
+module.exports.app = app;
