aa0af663570e8feb7900f95223c4549b0614dbaa
Ilja Kogan <ilja.kogan@gmail.com>
2017-06-22 20:23:30 +0000

    GET method added

--- server/server.js
+++ server/server.js
@@ -2,16 +2,41 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {
+    mongoose
+} = require('./db/mongoose');
+var {
+    Todo
+} = require('./models/todo');
+var {
+    User
+} = require('./models/user');
 
-var app=express();
+var app = express();
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res)=>{
-   console.log(req.body) ;
-});
 
-app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0");
-module.exports.app = app;
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    })
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+
+    });
+});
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", () => {
+    console.log('started');
+});
+//module.exports.app = app;
